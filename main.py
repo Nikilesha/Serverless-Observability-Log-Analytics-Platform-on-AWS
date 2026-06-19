e180b1f3840d5769b75bbdc14883aca8c1669750
Nikilehsa <nikilesha.nivas@gmail.com>
2026-06-19 15:52:52 +0530

    modified phase 2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from src.parser import parse_log_file
+from src.analyzer import run_analyzer
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+path1 = os.getenv("PATH1")
+path2 = os.getenv("PATH2")
+
+
+run_analyzer(path1,path2)
